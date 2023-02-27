9ed38bef4b2c6c244727864388c74a07e95487b0
castel <aitorcastel@gmail.com>
2023-02-27 06:35:14 +0000

    fix range

--- course_intro/favouriteByteSol.py
+++ course_intro/favouriteByteSol.py
@@ -2,7 +2,7 @@
 encoded_flag = '73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d'
 
 def main():
-    for iterator in range(1337): 
+    for iterator in range(256): 
         results = [chr(character^iterator) for character in bytes.fromhex(encoded_flag)]
         temp_flag = "".join(results)
         if(temp_flag.startswith('crypto')):
